e8a35ea3481ca7668a806893abb5dcb55d7f2991
Brendan-funk <brendanzfunk@gmail.com>
2022-02-07 22:32:58 +0000

    added assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+//test
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("hello world", "hello world");
+assertEqual(12,21);
