01bcf5044a3cfad7009346c8139f4712b2809470
Brendan-funk <brendanzfunk@gmail.com>
2022-02-09 23:41:08 +0000

    added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  const results = {};
+  const noSpace = str.replace(/\s/g, '');
+  const input = noSpace.toLowerCase();
+  for (const letter of input) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+
+  }
+  return results;
+};
+
+const test = countLetters('lighthouse in the house');
+assertEqual(test['l'],1);
+assertEqual(test['i'],2);
+assertEqual(test['g'],1);
+assertEqual(test['h'],4);
+assertEqual(test['t'],2);
+assertEqual(test['o'],2);
+assertEqual(test['u'],2);
+assertEqual(test['s'],2);
+assertEqual(test['e'],3);
+assertEqual(test['n'],1);
