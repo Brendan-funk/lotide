319fbe5b1b9948b4627348030d5d70184bd19209
Brendan-funk <brendanzfunk@gmail.com>
2022-02-10 02:16:15 +0000

    added assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,55 @@
+const eqObjects = function(obj1,obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key in obj1) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key],obj2[key])) {
+        return false;
+      }
+    } else if (typeof(obj1[key]) === 'object' && typeof(obj2[key]) === 'object') {
+      if (!eqObjects(obj1[key]),obj2[key]) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(obj1,obj2) {
+  const inspect = require('util').inspect;
+  if (eqObjects(obj1,obj2)) {
+    console.log(`✅ Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
+  }
+};
+
+const a = {
+  a:2,
+  b:3,
+};
+const b = {
+  b:4,
+  a:2,
+};
+
+assertObjectsEqual(a,b);
