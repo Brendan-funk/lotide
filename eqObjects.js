4c85074c75d93258641581bb3681787e06a5587e
Brendan-funk <brendanzfunk@gmail.com>
2022-02-10 01:53:09 +0000

    added array and object functionality to eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -6,14 +6,36 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const eqObjects = function(obj1,obj2) {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
+
   if (keys1.length !== keys2.length) {
     return false;
   }
+
   for (const key in obj1) {
-    if (obj1[key] !== obj2[key]) {
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+      if (!eqArrays(obj1[key],obj2[key])) {
+        return false;
+      }
+    } else if (typeof(obj1[key]) === 'object' && typeof(obj2[key]) === 'object') {
+      if (!eqObjects(obj1[key]),obj2[key]) {
+        return false;
+      }
+    } else if (obj1[key] !== obj2[key]) {
       return false;
     }
   }
@@ -26,3 +48,8 @@ const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba),true);
 const abc = {a: '1', b: '2', c: '3'};
 assertEqual(eqObjects(ab,abc),false);
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc),true);
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2),false);
