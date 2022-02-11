22ab23b5ff703d6de8f1ccbda063d217a19383f5
Brendan-funk <brendanzfunk@gmail.com>
2022-02-11 01:17:54 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const findKey = function(obj,func) {
+  for (const key in obj) {
+    if (func(obj[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const results = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(results, 'noma');
--- takeUntil.js
+++ takeUntil.js
@@ -9,6 +9,26 @@ const takeUntil = function(array,func) {
   return output;
 };
 
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return;
+  }
+  console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+};
+
 //test cases
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -19,3 +39,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArrayEqual(results1,[1,2,5,7,2]);
+assertArrayEqual(results2,["I've", "been", "to", "Hollywood"]);
